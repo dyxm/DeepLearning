b1a4d54670a51b8484404d8dab3379ca26868ad5
dyxm <857230132@qq.com>
2018-06-07 16:24:31 +0800

    RNN手写数字识别

---
+++ HandWritingClassification/RNN.py
@@ -0,0 +1,101 @@
+# Created by [Yuexiong Ding] at 2018/6/6
+# RNN 手写数字识别
+#
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib import rnn
+
+# 超参设置
+# 学习率
+lr = 0.001
+# 迭代次数
+iteration = 100000
+# 每轮训练数据的 batch_size = 128
+batch_size = 128
+
+# 神经网络参数设置
+# 输入层
+n_inputs = 28
+# 步数
+n_steps = 28
+# 隐层神经元个数
+n_hidden_units = 128
+# 输出类别个数
+n_claeese = 10
+
+# 输入数据集权重设置
+# 输入(图片为28*28)
+x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
+y = tf.placeholder(tf.float32, [None, n_claeese])
+# 权重
+weights = {
+    # (28, 128)
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
+    # (128, 10)
+    'out': tf.Variable(tf.random_normal([n_hidden_units, n_claeese]))
+}
+# 偏置值
+biases = {
+    # (128, )
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_units])),
+    # (10, )
+    'out': tf.Variable(tf.constant(0.1, shape=[n_claeese]))
+}
+
+
+# 定义 RNN 模型
+def RNN(X, w, b):
+    # (128, 28, 28) -> (128 * 28, 28)
+    X = tf.reshape(X, [-1, n_inputs])
+
+    # 进入隐层(128 * 28, 128)
+    X_in = tf.matmul(X, w['in']) + b['in']
+    # (128 * 28, 128) -> (128, 28, 128)
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
+
+    # LSTM 循环单元
+    lstm_cell = rnn.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
+    # init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
+    outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X_in, dtype=tf.float32, time_major=False)
+
+    results = tf.matmul(final_state[1], weights['out']) + biases['out']
+
+    return results
+
+
+# 预测输出
+pred = RNN(x, weights, biases)
+# 代价
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+# Adam 优化器
+train_op = tf.train.AdamOptimizer(lr).minimize(cost)
+
+# 准确率
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+with tf.Session() as sess:
+    # 读取数据
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+    # 初始化变量
+    tf.global_variables_initializer().run()
+
+    step = 0
+    while step * batch_size < iteration:
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        sess.run(train_op, feed_dict={x: batch_xs, y: batch_ys})
+        if step % 10 == 0:
+            train_accuracy = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys})
+            print('第 %d 次训练后准确率为 %f ' % (step, train_accuracy))
+
+        step += 1
+
+    test_xs = mnist.test.images
+    test_ys = mnist.test.labels
+    test_xs = test_xs.reshape([-1, n_steps, n_inputs])
+    test_accuracy = sess.run(accuracy, feed_dict={x: test_xs, y: test_ys})
+    print('********************************************')
+    print('模型最终准确率为 %f ' % test_accuracy)
